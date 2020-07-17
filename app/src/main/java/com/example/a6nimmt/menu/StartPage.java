f287e1fbc41b126a95764d4b1ee856b753040115
ghshenavar <ghshenavar@gmail.com>
2020-07-17 15:32:59 +0430

    exit

---
+++ app/src/main/java/com/example/a6nimmt/menu/StartPage.java
@@ -0,0 +1,58 @@
+package com.example.a6nimmt.menu;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.a6nimmt.R;
+
+import static androidx.core.app.ActivityCompat.finishAffinity;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StartPage extends Fragment {
+
+    public static Activity activity;
+
+    public StartPage() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start_page, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    public void startbutton(View view) {
+        //add fragment to get number of players
+    }
+
+    public void exitButton(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getActivity().finish();
+            System.exit(0);
+        }
+    }
+}
